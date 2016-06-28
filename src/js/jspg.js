b26e1edcf2c3153df4ca136576b7930051459a72
10Dozen <steel.mjr@gmail.com>
2016-06-28 20:38:39 +0300

    Add files via upload

--- src/js/jspg.js
+++ src/js/jspg.js
@@ -1,13 +1,16 @@
 var Scenes = {
-	"Scene1": {
+	"InitScene": {
+		"desc": "Init Scene is here"
+		, "actions": [
+			["Next", "console.log('Text executed')"]
+		]
+	}
+	,"Scene1": {
 		"desc": "- You chose a wrong door, leather man. Leather club is two blocks down!"
 		,"actions": [
 			["- Fuck You!", ""]
-			["Go away", ""]
+			,["Go away", ""]
 		]	
-	}
-	, "Scene2": {
-	
 	}
 };
 
@@ -19,15 +22,35 @@ var GamePrototype = function () {
 			"<div class='scene-description scene-even'>" 
 			+ Scene.desc
 			+ "</div>"
-		);
-		$(".actions").append( Scene.actions );
+		);		
+		
+		for (var i=0; i < Scene.actions.length; i++) {
+			$(".btn-" + (i + 1)).html( Scene.actions[i][0] );	
+			$(".btn-" + (i + 1)).css("display","block");
+			
+			$(".btn-" + (i + 1)).attr("toExecute", Scene.actions[i][1]);
+			
+			$(".btn-" + (i + 1)).off();
+			$(".btn-" + (i + 1)).on("click", function () {
+				eval( $(this).attr("toExecute") );			
+			});
+			
+		};		
 	};
+	
+	
+	this.hideActions = function () {
+		$(".action-btn").css("display","none");
+	};
+	
+	this.hideActions();
+	
 };
 
 $( document ).ready(function() {
 	Game = new GamePrototype();
 
-
+	Game.showScene(Scenes.InitScene);
 
 
 });

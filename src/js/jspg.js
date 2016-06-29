2feb8c898dd82335c6f0115211e4f383cf19a716
10Dozen <steel.mjr@gmail.com>
2016-06-29 15:23:21 +0300

    Update jspg.js

--- src/js/jspg.js
+++ src/js/jspg.js
@@ -1,5 +1,5 @@
 (function($) {
-    $.fn.goTo = function() {
+    $.fn.scrollTo = function() {
         $('html, body').animate({
             scrollTop: $(this).offset().top + 'px'
         }, 'fast');
@@ -13,6 +13,7 @@ var GamePrototype = function () {
 	this.toExecude = "";
 	this.currentScene = {};
 	
+	this.sceneDesc = "";
 	
 	this.goTo = function (SceneName) {
 		eval( "Game.showScene(Scenes." + SceneName + ")" )
@@ -21,24 +22,26 @@ var GamePrototype = function () {
 	
 	this.showScene = function (Scene) {	
 		this.currentScene = Scene;
+		this.sceneDesc = this.currentScene.desc;	
 		this.scenesCurrentId++;	
-		this.lastActionsHeight = 20;
 		
-		this.hideActions();
+		this.hideActions();		
+		this.execPreScene();
 		
 		// Scene
 		$block = "<div class='scene-description scene-even' sceneId='" 
 			+ (this.scenesCurrentId-1) + "'>" 
-			+ Scene.desc
+			+ this.sceneDesc
 			+ "</div>";
 		$("#scenes").append( $block	);
 		
+		// Show Scene
 		setTimeout(function () {
 			$(".scene-description[sceneId=" + (Game.scenesCurrentId-1) + "]").css("opacity", 1);
-			$(".scene-description[sceneId=" + (Game.scenesCurrentId-1) + "]").goTo();
+			$(".scene-description[sceneId=" + (Game.scenesCurrentId-1) + "]").scrollTo();
 		}, 1000);		
 		
-		// Actions
+		// Set Actions
 		for (var i=0; i < Scene.actions.length; i++) {
 			var answer = Scene.actions[i][0];
 			var showAnswer = Scene.actions[i][1];
@@ -65,8 +68,11 @@ var GamePrototype = function () {
 			
 			$(".actions").css("min-height", $(".actions").height() + "px");
 		};
+		
+		// Show Actions
 		setTimeout(function () {
 			$(".action-btn").css("opacity", 1);
+			Game.execPostScene();
 		}, 1500);	
 		
 	};
@@ -82,6 +88,12 @@ var GamePrototype = function () {
 		}, 500);
 	};
 	
+	this.execPreScene = function () {
+		eval( this.currentScene.exec.pre );
+	};
+	this.execPostScene = function () {
+		eval( this.currentScene.exec.post );
+	};
 	
 	this.hideActions = function () {
 		$(".actions").css("min-height", $(".actions").height() + "px");

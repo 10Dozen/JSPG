961b67869a5edff48988690d0e8f444b1136c8ec
10Dozen <steel.mjr@gmail.com>
2016-06-30 09:52:42 +0300

    Update jspg.js

--- src/js/jspg.js
+++ src/js/jspg.js
@@ -10,10 +10,13 @@
 
 var GamePrototype = function () {
 	this.scenesCurrentId = 0;
-	this.toExecude = "";
-	this.currentScene = {};
+	this.scenesCurrentFrame = 0;
+	this.scenesMaxFrame = 0;
 	
+	this.toExecude = "";
+	this.currentScene = {};	
 	this.sceneDesc = "";
+	this.sceneActions = [];
 	
 	this.goTo = function (SceneName) {
 		eval( "Game.showScene(Scenes." + SceneName + ")" )
@@ -21,30 +24,48 @@ var GamePrototype = function () {
 	};
 	
 	this.showScene = function (Scene) {	
-		this.currentScene = Scene;
+		this.currentScene = Scene;		
 		this.sceneDesc = Scene.desc;
 		this.sceneActions = Scene.actions;
 		this.scenesCurrentId++;	
 		
-		this.hideActions();		
+		this.hideActions();
+		
+		if (this.sceneDesc.constructor !== Array) {			
+			this.sceneDesc = [this.sceneDesc];
+		}
+		
 		this.execPreScene();
 		
 		// Scene
-		$block = "<div class='scene-description scene-even' sceneId='" 
-			+ (this.scenesCurrentId-1) + "'>" 
-			+ this.sceneDesc
-			+ "</div>";
-		$("#scenes").append( $block	);
-		
-		// Show Scene
-		setTimeout(function () {
-			$(".scene-description[sceneId=" + (Game.scenesCurrentId-1) + "]").css("opacity", 1);
-			$(".scene-description[sceneId=" + (Game.scenesCurrentId-1) + "]").scrollTo();
-		}, 1000);		
+		this.scenesCurrentFrame = 0;
+		this.scenesMaxFrame = this.sceneDesc.length;
+		for (var i=0; i < this.sceneDesc.length; i++) {
+			$block = "<div class='scene-description scene-even'"
+			+ " sceneId='" 	+ (this.scenesCurrentId-1) + "'"
+			+ " sceneFrame='" + i + "'>" 
+			+ this.sceneDesc[i] + "</div>";
+			
+			$("#scenes").append( $block	);
+			
+			// Show Scene
+			setTimeout(function () {
+				$(".scene-description[sceneId=" + (Game.scenesCurrentId-1) + "][sceneFrame=" + Game.scenesCurrentFrame + "]").css("opacity", 1);
+				$(".scene-description[sceneId=" + (Game.scenesCurrentId-1) + "][sceneFrame=" + Game.scenesCurrentFrame + "]").scrollTo();
+				
+				Game.scenesCurrentFrame++;
+				
+				// Show Actions
+				if (Game.scenesCurrentFrame >= Game.scenesMaxFrame) {
+					Game.showActions()					
+				}
+			}, 500 + 2000*i);
+		}
 		
 		// Set Actions
 		for (var i=0; i < this.sceneActions.length; i++) {
 			var $btn = ".btn-" + (i + 1);
+			
 			var shortAnswer, fullAnswer;
 			if ( (this.sceneActions[i][0]).constructor === Array ) {
 				shortAnswer = this.sceneActions[i][0][0];
@@ -61,6 +82,9 @@ var GamePrototype = function () {
 		
 			$( $btn ).html( shortAnswer );	
 			$( $btn ).css("display","block");
+			
+			$( $btn ).css("position","absolute");
+			$( $btn ).css("left","-200px");
 			
 			$( $btn ).attr("toExecute", toExecute);	
 			$( $btn ).attr("showDesc", fullAnswer);
@@ -80,11 +104,8 @@ var GamePrototype = function () {
 			$(".actions").css("min-height", $(".actions").height() + "px");
 		};
 		
-		// Show Actions
-		setTimeout(function () {
-			$(".action-btn").css("opacity", 1);
-			Game.execPostScene();
-		}, 1500);	
+		
+			
 		
 	};
 	
@@ -106,6 +127,15 @@ var GamePrototype = function () {
 		eval( this.currentScene.exec.post );
 	};
 	
+	this.showActions = function () {		
+		setTimeout(function () {
+			$(".action-btn").css("position","");
+			$(".action-btn").css("left","0px");
+			
+			$(".action-btn").css("opacity", 1);
+			Game.execPostScene();
+		}, 1500);		
+	};
 	this.hideActions = function () {
 		$(".actions").css("min-height", $(".actions").height() + "px");
 		

22b7a21dcb803e7ed320b33974e13b0dcc25edfc
10Dozen <steel.mjr@gmail.com>
2018-03-05 22:21:02 +0300

    Update DataTypes.js

--- dev/DataTypes.js
+++ dev/DataTypes.js
@@ -1,5 +1,6 @@
-//  Title scene
-
+//
+// Title scene
+//
 "InitScene": {
 	"type":"Title"
 	,"desc":"Game Title Name"
@@ -9,19 +10,63 @@
 	}
 }
 
+"InitScene": {
+	"type":"Title"
+	,"desc": [
+		"Game Title Name"
+		, "Another title line for any reason"
+	]
+	,"exec":{
+		"pre":"Game.myVar = 1"
+		, "post":"Game.goTo('Intro')"
+	}
+}
+
+//
+//  Subtitle scene
+//
+"InitScene": {
+	"type":"Subitle"
+	,"desc":"Game Title Name"
+	,"exec":{
+		"pre":"Game.myVar = 2"
+		, "post":"Game.goTo('Intro')"
+	}
+}
+
+//
 // Simple scene
+//
 ,"Intro": {
 	"type":"Scene"
 	,"desc":[
 		"Some description text here in first blob"
 		,"Another description text here in second blob"
 	]
-	,"actions":[
-		{
-			"name":"Displayed Action Name"
-			,"exec":"Game.goTo('SceneHub');"
-			,"type":"Hidden"
-		}
-	]
+	,"actions":[ ... ]
 }
 
+//
+// Dialog scene
+//
+,"Dialog1": {
+	"type":"Dialog"
+	,"desc":[
+		"Some description text here in first blob"
+		,"Another description text here in second blob"
+	]
+	,"portrait":"img/character1.jpg"
+	,"actions":[ ... ]
+}
+		    
+	
+//
+// Actions
+//    
+		    
+		    
+{
+	"name":"Displayed Action Name"
+	,"exec":"Game.goTo('SceneHub');"
+	,"type":"Hidden"
+}
